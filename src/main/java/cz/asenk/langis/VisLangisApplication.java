0af25031cd41487be5f5f9f7e653a39f16da240e
Senkyrik Alois <asenkyrik@monetplus.cz>
2022-12-07 12:55:20 +0100

    Initial commit

---
+++ src/main/java/cz/asenk/langis/VisLangisApplication.java
@@ -0,0 +1,13 @@
+package cz.asenk.langis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisLangisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VisLangisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/asenk/langis/VisLangisApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.asenk.langis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisLangisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
