ff8799ec2458ce91c066516b04ff968188a143c6
Senkyrik Alois <asenkyrik@monetplus.cz>
2022-12-12 13:54:40 +0100

    Added basic API

---
+++ modules/server-api/src/main/java/cz/asenk/vsb/langis.api/LangisAPIV1.java
@@ -0,0 +1,9 @@
+package cz.asenk.vsb.langis.api;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public interface LangisAPIV1 {
+
+}
---
+++ modules/server-api/src/main/java/cz/asenk/vsb/langis/model/SometingWrapper.java
@@ -0,0 +1,15 @@
+package cz.asenk.vsb.langis.model;
+
+import lombok.Builder;
+
+/**
+ * @author Alois Šenkyřík asenkyrik@monetplus.cz
+ * @version 1.0
+ * Created: 12.12.2022
+ */
+
+@Builder
+public record SometingWrapper(
+
+) {
+}
